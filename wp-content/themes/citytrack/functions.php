be9859a8593f25c92150e30f486cdc96b5a5bed2
Máté Kis-Király <Mt Kis-Kirly>
2016-11-07 09:37:58 +0200

    Add footer menu element - option page

--- wp-content/themes/citytrack/functions.php
+++ wp-content/themes/citytrack/functions.php
@@ -52,6 +52,15 @@ if( function_exists('acf_add_options_page') ) {
         'capability'    => 'edit_posts',
         'redirect'      => false
     ));
+
+
+    acf_add_options_page(array(
+        'page_title'    => 'CityTrack footer',
+        'menu_title'    => 'CityTrack footer',
+        'menu_slug'     => 'footer',
+        'capability'    => 'edit_posts',
+        'redirect'      => false
+    ));
     
 }
 
