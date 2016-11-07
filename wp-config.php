1537f0436d3fab1824ed2d56bbb08777c57a0db3
Máté Kis-Király <Mt Kis-Kirly>
2016-11-07 09:55:18 +0200

    Disable Theme Editor

--- wp-config.php
+++ wp-config.php
@@ -79,6 +79,11 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+/**
+* Disable Theme Editor
+*/
+define( 'DISALLOW_FILE_EDIT', true );
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
