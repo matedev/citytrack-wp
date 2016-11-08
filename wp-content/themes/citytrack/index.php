3b82901a9b5377a723c8935fffdf0c23378c6082
Máté Kis-Király <Mt Kis-Kirly>
2016-11-08 16:42:08 +0200

    Edit index page from WP

--- wp-content/themes/citytrack/index.php
+++ wp-content/themes/citytrack/index.php
@@ -18,35 +18,16 @@ get_header();
     </div>
 
     <div id="block1" class="block">
-        <div class="container">
-            <div class="row">
-                <h2 class="text-center text-blue col-lg-6 col-lg-push-3 col-md-8 col-md-push-2 col-sm-10 col-sm-push-1 col-xs-12">
-                    Indoor and outdoor positioning in Tampere city centre area
-                </h2>
-            </div>
-                <p class="text-center text-light">
-                    CityTrack project will become an unique urban test site for the design, development and piloting of modern 
-                    mobility services and service-rich dense urban venues. CityTrack helps companies create a highly personalized,
-                    seamless and data-driven cross-channel event and shopping experience at Tampere station area 
-                    both indoors and outdoors.
-                </p>
-            
+        <div class="container text-center">
+            <?php the_field('block_1'); ?>
         </div>
     </div>
 
     <div id="block2" class="block">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 padding-t60 padding-b60">
-                    <p class="text-center margin-0 text-light">
-                        CityTrack provides tools and analyzed information for venue owners to enhance their 
-                        services in dense urban areas. This is achieved through combining contextual awareness 
-                        of location data, traffic and people flow which includes open traffic data, customer
-                        preferences, and customer engagement. <span class="text-blue text-regular"> In Tampere 
-                        city centre around the railway station </span> CityTrack area includes <span class="text-blue text-regular"> 
-                        shopping malls, parking halls, event venues, university campus, 
-                        tourist attractions, and conference facilities. </span>
-                    </p>
+                <div class="col-md-6 padding-t60 padding-b60 text-center">
+                    <?php the_field('block_2'); ?>
                 </div>
                 <div class="col-md-6 col-sm-12 padding-t60 padding-b60">
                     <div class="embed-video embed-responsive embed-responsive-16by9">
@@ -60,13 +41,9 @@ get_header();
     <div id="block3" class="block">
          <div class="container">
             <div class="row">
-                 <p class="text-center col-sm-12 text-light">
-                     The positioning works indoors with wireless Internet, radio and bluetooth technology. 
-                     The system uses Here positioning software. Therefore CityTrack offers seamless outdoor
-                      to indoors positioning, routing and maps. CityTrack also counts in bus and train 
-                      traffic, events, shopping times, and weather. The services and applications developed 
-                      in the area can be exported to any other location using Here assets.
-                 </p>
+                <div class="col-sm-12 text-center">
+                    <?php the_field('block_2'); ?>
+                </div>
             </div>
         </div>
     </div>
