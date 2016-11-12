52344188949c07dc870e699e7afac800957ccceb
Máté Kis-Király <Mt Kis-Kirly>
2016-11-12 22:26:49 +0200

    Index pixel fix

--- wp-content/themes/citytrack/index.php
+++ wp-content/themes/citytrack/index.php
@@ -13,10 +13,11 @@ get_header();
         </div>  
     </div>
 
+    <div class="container separator">
+        <div class="separator-content"><img src="<?php echo get_template_directory_uri()?>/assets/img/logo.png" height="64px"></div>
+    </div>
+
     <div id="block1" class="block">
-        <div class="container separator">
-                <h1 class="separator-content"><img src="<?php echo get_template_directory_uri()?>/assets/img/logo.png" height="63px"></h1>
-        </div>
         <div class="container text-center">
             <?php the_field('block_1'); ?>
         </div>
@@ -43,7 +44,7 @@ get_header();
     <div id="block2" class="block">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 padding-t60 padding-b60 text-center">
+                <div class="col-md-6 padding-50 text-center">
                     <?php the_field('block_2'); ?>
                 </div>
                 <div class="col-md-6 col-sm-12 padding-t60 padding-b60">
