aa09ef89b7662c4ce0b5acf203bea8e520d73a15
Máté Kis-Király <Mt Kis-Kirly>
2016-11-22 10:05:10 +0200

    Force SSL login

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,9 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+/** Force SSL logins */
+define('FORCE_SSL_ADMIN', true);
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
